f0dafd60e80eb303874e1085ab0161d15b1c455e
Jorge Sirias <jsiri004@fiu.edu>
2019-11-20 19:31:44 -0500

    initial commit

---
+++ Lab1JorgeSirias/FileUpload.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="FileUpload.aspx.cs" Inherits="FileUpload" %>
+
+<!DOCTYPE html>
+<!-- 
+  Author:Jorge Sirias
+  COP 4813 Website Application Programming
+  
+  -->
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Jorge Sirias Lab 1</title>
+    <link href="CSS/StyleSheet.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="Main">
+            <br />
+            <br />
+            Lab 1
+        </div>
+        <br />
+        <br />
+        <div>
+            <span style="color:white; font-size: 20px">File Upload<br />
+            </span>
+        &nbsp;<asp:FileUpload ID="FileUploadControl" runat="server" BackColor="White"/>
+            <br />
+            <asp:Button ID="UploadButton" text="Upload" runat="server" OnClick="UploadButton_Click"/>
+            <asp:Button ID="CancelButton" Text="Cancel" runat="server"/>
+            <br />
+            <br />
+            <div id="Grid" runat="server" visible="true"><asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false">
+               <Columns>
+                   <asp:BoundField DataField="Text" HeaderText="File Name" />
+                   <asp:BoundField DataField="Text" HeaderText="File Size" />
+                   <asp:BoundField DataField="Text" HeaderText="Date Modified" />
+            </Columns>
+            </asp:GridView></div>
+            <div id="Outer" runat="server" visible="true">
+                <div id="MessageBox" runat="server" visible="true">
+                    <asp:Label ID="Label1" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lab1JorgeSirias/FileUpload.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+/*
+  Author:Jorge Sirias
+  COP 4813 Website Application Programming
+ */ 
+ 
+
+public partial class FileUpload : System.Web.UI.Page
+{
+    String fileName;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Outer.Visible = false;
+        MessageBox.Visible = false;
+
+        if (Directory.GetFileSystemEntries(Server.MapPath("~/Files/")).Length == 0)
+        {
+            Outer.Visible = true;
+            MessageBox.Visible = true;
+            Label1.Text = "No files were found";
+        }
+        else
+        {
+            Grid.Visible = true;
+            //if (!IsPostBack)
+            //{
+            string[] directoryFiles = Directory.GetFiles(Server.MapPath("~/Files/" + fileName));
+            List<ListItem> files = new List<ListItem>();
+            foreach (string filePath in directoryFiles)
+            {
+                files.Add(new ListItem(Path.GetFileName(filePath), filePath));
+                //files.Add(new ListItem(filePath.Length.ToString()));
+            }
+            GridView1.DataSource = files;
+            GridView1.DataBind();
+            //}
+        }
+    }
+    protected void UploadButton_Click(object sender, EventArgs e)
+    {
+        if(FileUploadControl.HasFile)
+        {
+            int count = 0;
+            int index;
+            string temp;
+
+            fileName = FileUploadControl.FileName;
+
+            while(File.Exists(Server.MapPath("~/Files/" + fileName)))
+            {
+                count++;
+                index = fileName.IndexOf('.');
+                //index -= 1;
+                fileName = fileName.Insert(index,count.ToString());
+                //fileName += count.ToString();
+            }
+            
+            FileUploadControl.SaveAs(Server.MapPath("~/Files/"+ fileName));
+        }
+        else
+        {
+            Outer.Visible = true;
+            MessageBox.Visible = true;
+            Label1.Text = "Please choose a file and try again";
+        }
+    }
+}
